3305caa4b35a3cc88c8a582172777c9e6b80ddf4
kjminn <kjminn2001@gmail.com>
2022-10-11 00:43:09 +0900

    minSdk 31로 변경함

--- app/src/main/java/kr/ssu/dongyang/minn/login/LoginActivity.java
+++ app/src/main/java/kr/ssu/dongyang/minn/login/LoginActivity.java
@@ -58,11 +58,11 @@ public class LoginActivity extends AppCompatActivity {
                             // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
                             System.out.println("hongchul" + response);
                             JSONObject jsonObject = new JSONObject(response);
+
                             boolean success = jsonObject.getBoolean("success");
                             if (success) { // 로그인에 성공한 경우
                                 String userID = jsonObject.getString("userID");
                                 String userPass = jsonObject.getString("userPassword");
-
                                 Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 intent.putExtra("userID", userID);
